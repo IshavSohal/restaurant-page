64f8dc4ff8898187a4be282581f33bef1f420059
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2025-05-24 22:06:59 -0400

    add contact page

---
+++ src/contact.js
@@ -0,0 +1,146 @@
+import spectacularSmoothie from "./spectacular-smoothie-bar.png";
+
+export const addContactContent = () => {
+    const contentDiv = document.querySelector(".content");
+
+    // ADD MAIN TITLE
+    const mainTitle = document.createElement("h1");
+    mainTitle.textContent = "Contact";
+    mainTitle.setAttribute("id", "content-title");
+    contentDiv.appendChild(mainTitle);
+
+    // ADD VISIT US CONTENT
+    const visitTitle = document.createElement("h2");
+    visitTitle.textContent = "Visit us";
+    contentDiv.appendChild(visitTitle);
+
+    const visitUs = document.createElement("div");
+    visitUs.setAttribute("class", "location-container");
+
+    // Add image container
+    const imgContainer = document.createElement("div");
+    const locationImg = document.createElement("img");
+    locationImg.setAttribute("src", spectacularSmoothie);
+    imgContainer.appendChild(locationImg);
+    visitUs.appendChild(imgContainer);
+
+    // Add contact info container
+    const gridContactContainer = document.createElement("div");
+    const contactContainer = document.createElement("div");
+    const address = document.createElement("div");
+    address.textContent = "123 Plae Grownd Stret";
+    contactContainer.appendChild(address);
+    const location = document.createElement("div");
+    location.textContent = "Toronto, Ontario M3C 3B7";
+    contactContainer.appendChild(location);
+    const phoneNum = document.createElement("div");
+    phoneNum.textContent = "P: 647-637-9987";
+    contactContainer.appendChild(phoneNum);
+    gridContactContainer.appendChild(contactContainer);
+
+    visitUs.appendChild(gridContactContainer);
+    contentDiv.appendChild(visitUs);
+
+    // ADD SEND MESSAGE CONTENT
+    const sendMessageTitle = document.createElement("h2");
+    sendMessageTitle.textContent = "Send us a message";
+    contentDiv.appendChild(sendMessageTitle);
+
+    // Add send message form
+    const formContainer = document.createElement("div");
+    formContainer.setAttribute("class", "form-container");
+    const messageForm = document.createElement("form");
+    messageForm.setAttribute("method", "");
+    messageForm.setAttribute("action", "");
+    messageForm.setAttribute("id", "send-message");
+
+    // Create form row 1
+    const formRow1 = document.createElement("div");
+    formRow1.setAttribute("class", "form-row");
+
+    // Create first name container
+    const firstNameContainer = document.createElement("div");
+    const firstNameLabel = document.createElement("label");
+    firstNameLabel.setAttribute("for", "firstName");
+    firstNameLabel.textContent = "First Name";
+    firstNameContainer.appendChild(firstNameLabel);
+    const firstNameInput = document.createElement("input");
+    firstNameInput.setAttribute("type", "text");
+    firstNameInput.setAttribute("id", "firstName");
+    firstNameInput.setAttribute("placeholder", "Bob");
+    firstNameContainer.appendChild(firstNameInput);
+    formRow1.appendChild(firstNameContainer);
+
+    // Create last name container
+    const lastNameContainer = document.createElement("div");
+    const lastNameLabel = document.createElement("label");
+    lastNameLabel.setAttribute("for", "lastName");
+    lastNameLabel.textContent = "Last Name";
+    lastNameContainer.appendChild(lastNameLabel);
+    const lastNameInput = document.createElement("input");
+    lastNameInput.setAttribute("type", "text");
+    lastNameInput.setAttribute("id", "lastName");
+    lastNameInput.setAttribute("placeholder", "Smith");
+    lastNameContainer.appendChild(lastNameInput);
+    formRow1.appendChild(lastNameContainer);
+    messageForm.appendChild(formRow1);
+
+    // Create form row 2
+    const formRow2 = document.createElement("div");
+    formRow2.setAttribute("class", "form-row");
+
+    // Create email container
+    const emailContainer = document.createElement("div");
+    const emailLabel = document.createElement("label");
+    emailLabel.setAttribute("for", "email");
+    emailLabel.textContent = "Email";
+    emailContainer.appendChild(emailLabel);
+    const emailInput = document.createElement("input");
+    emailInput.setAttribute("type", "email");
+    emailInput.setAttribute("id", "email");
+    emailInput.setAttribute("placeholder", "bob@gmail.com");
+    emailContainer.appendChild(emailInput);
+    formRow2.appendChild(emailContainer);
+
+    // Create phone number container
+    const phoneContainer = document.createElement("div");
+    const phoneLabel = document.createElement("label");
+    phoneLabel.setAttribute("for", "phoneNumber");
+    phoneLabel.textContent = "Phone Number";
+    phoneContainer.appendChild(phoneLabel);
+    const phoneInput = document.createElement("input");
+    phoneInput.setAttribute("type", "tel");
+    phoneInput.setAttribute("id", "phoneNumber");
+    phoneInput.setAttribute("placeholder", "6474435567");
+    phoneContainer.appendChild(phoneInput);
+    formRow2.appendChild(phoneContainer);
+    messageForm.appendChild(formRow2);
+
+    // Create form row 3
+    const formRow3 = document.createElement("div");
+    formRow3.setAttribute("class", "form-row");
+
+    // Create message input container
+    const messageContainer = document.createElement("div");
+    const messageLabel = document.createElement("label");
+    messageLabel.setAttribute("for", "message");
+    messageLabel.textContent = "Message";
+    messageContainer.appendChild(messageLabel);
+    const messageTextArea = document.createElement("textarea");
+    messageTextArea.setAttribute("name", "message");
+    messageTextArea.setAttribute("id", "message");
+    messageContainer.appendChild(messageTextArea);
+    formRow3.appendChild(messageContainer);
+    messageForm.appendChild(formRow3);
+
+    formContainer.appendChild(messageForm);
+
+    // Create submit button
+    const submitButton = document.createElement("button");
+    submitButton.setAttribute("class", "form-submit");
+    submitButton.setAttribute("form", "send-message");
+    submitButton.textContent = "Send Message";
+    formContainer.appendChild(submitButton);
+
+    contentDiv.appendChild(formContainer);
+};
--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import "./style.css";
 import { addHomeContent } from "./home.js";
 import { addMenuContent } from "./menu.js";
+import { addContactContent } from "./contact.js";
 
 const contentDiv = document.querySelector(".content");
 const homeButton = document.querySelector("#home");
 const menuButton = document.querySelector("#menu");
-const aboutButton = document.querySelector("#about");
+const contactButton = document.querySelector("#contact");
 
 homeButton.addEventListener("click", () => {
     homeButton.setAttribute("class", "active-header-button");
     menuButton.removeAttribute("class");
-    aboutButton.removeAttribute("class");
+    contactButton.removeAttribute("class");
     contentDiv.innerHTML = "";
     addHomeContent();
 });
 
-aboutButton.addEventListener("click", () => {
-    aboutButton.setAttribute("class", "active-header-button");
+contactButton.addEventListener("click", () => {
+    contactButton.setAttribute("class", "active-header-button");
     menuButton.removeAttribute("class");
     homeButton.removeAttribute("class");
+    contentDiv.innerHTML = "";
+    addContactContent();
 });
 
 menuButton.addEventListener("click", () => {
     menuButton.setAttribute("class", "active-header-button");
     homeButton.removeAttribute("class");
-    aboutButton.removeAttribute("class");
+    contactButton.removeAttribute("class");
     contentDiv.innerHTML = "";
     addMenuContent();
 });
 
-const clickEvent = new Event("click");
-homeButton.dispatchEvent(clickEvent);
+// const clickEvent = new Event("click");
+// homeButton.dispatchEvent(clickEvent);
